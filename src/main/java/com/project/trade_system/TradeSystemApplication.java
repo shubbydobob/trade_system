86b5f7e61792942dd37a462cc811fb9371057976
WIN-VL94OSS7KEA\김상현 <shubbydobob@gmail.com>
2026-01-15 00:15:16 +0900

    inital commit

---
+++ src/main/java/com/project/trade_system/TradeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.trade_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/trade_system/TradeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.trade_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
